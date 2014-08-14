ce68adb33f0288e868d7ce58f07eaa1b4ce0c2ec
sandip ghadage <ghadagesandip@gmail.com>
2014-08-14 20:30:12 +0530

    created html and services for roles module and done html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 var app = angular.module('project', ['ngRoute'])
 
-    app.value('baseUrl','http://localhost/laravel/public/');
+    app.value('baseUrl','http://localhost/BugTrackerApp/public/');
     app.config(function($routeProvider) {
         $routeProvider
             .when('/', { title:"Dashboard", controller:'DashboardCtrl', templateUrl:'Views/dashboard.html' })
+
             .when('/roles',{title:"Role",controller:'ListRoleCtrl',templateUrl:'Views/Roles/listroles.html'})
+            .when('/add-new-role',{title:"Add Role",controller:'AddRoleCtrl',templateUrl:'Views/Roles/add-role.html'})
+            .when('/view-role/:roleId',{title:"Add Role",controller:'ViewRoleCtrl',templateUrl:'Views/Roles/view-role.html'})
+            .when('/update-role/:roleId',{title:"Update Role",controller:"UpdateRole",templateUrl:'Views/Roles/update-role.html'})
+
             .when('/users',{title:"Users",controller:'ListUsersCtrl',templateUrl:'Views/Users/listusers.html'})
             .when('/projects',{title:"Project",controller:'ListProjectCtrl',templateUrl:'Views/Projects/listprojects.html'})
             .when('/bug-types',{title:"Bug types",controller:'ListBugTypeCtrl',templateUrl:'Views/BugTypes/listbugtypes.html'})
@@ -12,7 +17,6 @@ var app = angular.module('project', ['ngRoute'])
             .when('/bugs',{title:"Bugs",controller:"BugCtrl",templateUrl:'Views/Bugs/listbugs.html'})
             .otherwise({redirectTo:'/'});
     });
-
 
 
     app.run(['$location', '$rootScope', function($location, $rootScope) {
@@ -27,7 +31,13 @@ var app = angular.module('project', ['ngRoute'])
     app.factory('RoleFactory', function ($http,baseUrl) {
         return{
             getRoles:function(){
-                return $http.get(baseUrl+'roles/getRolesList');
+                return $http.get(baseUrl+'getRolesList');
+            },
+            getRole:function(id){
+                return $http.get(baseUrl+'getRole/'+id);
+            },
+            saveRole : function(post){
+                return $http.post(baseUrl+'savePost',post)
             }
         }
     });
@@ -36,10 +46,15 @@ var app = angular.module('project', ['ngRoute'])
     app.factory('UserFactory',function($http,baseUrl){
        return {
            getUsers : function(){
-               return $http.get(baseUrl+'users/getUsers');
+               return $http.get(baseUrl+'getUsers');
            }
        }
     });
+
+
+
+
+
 
     app.controller('DashboardCtrl', ['$scope',function($scope) {
 
@@ -56,6 +71,42 @@ var app = angular.module('project', ['ngRoute'])
                     $scope.status = 'Unable to load customer data: ' + error.message;
                 });
 
+        }
+    }]);
+
+    app.controller('AddRoleCtrl',['$scope','RoleFactory',function($scope,RoleFactory){
+
+    }]);
+
+    app.controller('ViewRoleCtrl',['$scope','$routeParams','$location','RoleFactory',function($scope,$routeParams,$location,RoleFactory){
+
+        RoleFactory.getRole($routeParams.roleId)
+            .success(function(result){
+                $scope.role = result;
+            })
+            .error(function(error){
+               console.log(error);
+            });
+    }]);
+
+
+
+    app.controller('UpdateRole',['$scope','$routeParams','$location','RoleFactory',function($scope,$routeParams,$location,RoleFactory){
+
+        RoleFactory.getRole($routeParams.roleId)
+            .success(function(result){
+                    $scope.role = result;
+            })
+            .error(function(error){
+
+            });
+
+        $scope.saveRole = function(){
+            RoleFactory.saveRole($scope.role)
+                .success(function(){
+                    $scope.role = {}
+                    $location.path('/');
+                })
         }
     }]);
 
