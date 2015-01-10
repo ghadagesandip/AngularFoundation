eccd35346efdf043f6c463c0ff52ff327b19ee22
sandip home <ghadagesandip@gmail.com>
2015-01-10 23:39:22 +0530

    updated index method,updated add and view page and view controller

--- js/controllers/ViewTodoCtrl.js
+++ js/controllers/ViewTodoCtrl.js
@@ -1,8 +1,7 @@
 app.controller('ViewTodoCtrl',['$scope','$routeParams','TodoFactory',function($scope,$routeParams,TodoFactory){
     TodoFactory.getTodo($routeParams.id)
         .success(function(data,status,headers,config){
-            console.log(data);
-            $scope.todo = data;
+            $scope.todo = data.todo;
         })
         .error(function(data,status,headers,config){
 
