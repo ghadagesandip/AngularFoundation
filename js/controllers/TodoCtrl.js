d9504a4062051d096af4826652f2e92419ea4058
sandip ghadage <ghadagesandip@gmail.com>
2015-03-02 20:10:29 +0530

    created edit and delete task fuctionality

--- js/controllers/TodoCtrl.js
+++ js/controllers/TodoCtrl.js
@@ -16,18 +16,18 @@ app.controller('TodoCtrl',['$scope','$location','$routeParams','loginFact','Todo
                 $scope.status = 'Error occured while fetching data';
             })
 
-        $scope.deleteTodo = function(id){
-            alert(id); return false;
-            var title = $scope.todos[id].title;
+        $scope.deleteTodo = function(id,index){
+
+            var title = $scope.todos[index].title;
             TodoFactory.deleteTodo(this.todo.id)
                 .success(function(data,status,headers,config){
                     if(status ==200){
                         $scope.todos.splice(id, 1);
-                        $scope.status = "Delete todo : "+title;
+                        $scope.status = "Deleted todo : "+title+"  ";
                     }
                 })
                 .error(function(data,status,headers,config){
-
+                alert('error occured');
                 });
 
         };
